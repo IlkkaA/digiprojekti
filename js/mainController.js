56961a75ec84809dc93c059d3308e3763fb9086a
Ilkka Alakarhu <ilkka.alakarhu@gmail.com>
2017-05-12 15:40:55 +0300

    controlleriin ja kysymyksiin muutoksia

--- js/mainController.js
+++ js/mainController.js
@@ -1,60 +1,28 @@
-routerApp.controller('mainController', function($scope) {
+routerApp.controller('mainController', function($scope, $http) {
   
-  var myObj, myJSON, text, obj; 
-  myObj = {
-    "id": 1, "teksti": "HHkysely", "tyyppi": "koulukysely", "tila": "avoin", "kysymykset":
-    [
-      {"id" :1, "teksti": "Oletko kiinnostunut osallistumaan monialaiselle kurssille?", "tyyppiid": 2, "vaihtoehdot": 
-        [
-          {"id":1, "teksti": "Erittäin paljon"},
-          {"id":2, "teksti": "Jonkin verran"},
-          {"id":3, "teksti": "En juurikaan"},
-          {"id":4, "teksti": "En yhtään"}
-        ]
-      },
-      {"id": 2, "teksti": "Minkä koulutusohjelmien kanssa haluaisit yhteisen kurssin?", "tyyppiid": 1, "vaihtoehdot": 
-        [
-          {"id": 1, "teksti": "Finanssi- ja talousasiantuntija"},
-          {"id": 2, "teksti": "Johdon assistenttityö"},
-          {"id": 3, "teksti": "Journalismi"},
-          {"id": 4, "teksti": "Liiketalous"},
-          {"id": 5, "teksti": "Myyntityö"},
-          {"id": 6, "teksti": "Ruokatuotannon johtaminen"},
-          {"id": 7, "teksti": "Tietojenkäsittely"},
-        ]
-      },
-      {"id": 3, "teksti": "Ajatuksia monialaisen kurssin kehittämiseksi:", "tyyppiid": 3, "vaihtoehdot": null}
-    ]
-  };
-  myJSON = JSON.stringify(myObj);
-  localStorage.setItem("teksti", myJSON);
-
-  text = localStorage.getItem("teksti");
-  obj = JSON.parse(text);
-//  console.log(obj);
-//  console.log(obj.kysymykset[2]);
-  
-  $scope.question1 = obj.kysymykset[0].teksti;
-  $scope.question2 = obj.kysymykset[1].teksti;
-  $scope.question3 = obj.kysymykset[2].teksti;
-  
-  $scope.q1_v1 = obj.kysymykset[0].vaihtoehdot[0].teksti;
-  $scope.q1_v2 = obj.kysymykset[0].vaihtoehdot[1].teksti;
-  $scope.q1_v3 = obj.kysymykset[0].vaihtoehdot[2].teksti;
-  $scope.q1_v4 = obj.kysymykset[0].vaihtoehdot[3].teksti;
-  
-  $scope.q2_v1 = obj.kysymykset[1].vaihtoehdot[0].teksti;
-  $scope.q2_v2 = obj.kysymykset[1].vaihtoehdot[1].teksti;
-  $scope.q2_v3 = obj.kysymykset[1].vaihtoehdot[2].teksti;
-  $scope.q2_v4 = obj.kysymykset[1].vaihtoehdot[3].teksti;
-  $scope.q2_v5 = obj.kysymykset[1].vaihtoehdot[4].teksti;
-  $scope.q2_v6 = obj.kysymykset[1].vaihtoehdot[5].teksti;
-  $scope.q2_v7 = obj.kysymykset[1].vaihtoehdot[6].teksti;
+  //get kutsu
+  $http.get('http://proto333.haaga-helia.fi:8080/mvc/kyselyt/1/kysymykset/').
+    then(function(response) {
+      $scope.obj = response.data;
+      
+      console.log($scope.obj);
+    });
   
   $scope.formData = {};
-  
+  //post
   $scope.submitForm = function(){
     console.log('tulokset',$scope.formData);
+    
+    var sendData = {'id': $scope.obj.id, 'vastaukset': [
+      {'id': $scope.obj.kysymykset[0].id,
+       'kysymysid': $scope.obj.kysymykset[0].tyyppiid,
+       'teksti': null,
+       'vaihtoehdot': [
+         {'id': 2, 'teksti': $scope.formData.teksti}
+       ]
+      }
+    ]};
+    console.log(sendData);
 //    $http({
 //      method : 'POST',
 //      url : 'end.html',
@@ -63,20 +31,10 @@ routerApp.controller('mainController', function($scope) {
 //    }).then(function(response) {
 //        data = response.data;
 //      console.log("tulokset", data);
-//    });
-   
-    //.success(function(data) { })
-    
-
-    //var data = $scope.fields;
-    //console.log(data);
-//    $http.get("data.json")
-//      .then(function(response) {
-//      $scope.fields = response.data;
 //    })
-    
-    //$http.post(xxx, data);
+//   
+//    .success(function(data) { })
   };
+    
   
-});
-
+}); //controller ends
