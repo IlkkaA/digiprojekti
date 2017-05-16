2b3c71bad70b8ecf546d951247cab8f056c627a4
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2017-05-16 13:46:53 +0300

    quest 1 ja 2 inputit tulee ng-repeatin avulla, ctrl:ssa läh. obj kuntoon

--- js/mainController.js
+++ js/mainController.js
@@ -1,28 +1,78 @@
 routerApp.controller('mainController', function($scope, $http) {
   
-  //get kutsu
-  $http.get('http://proto333.haaga-helia.fi:8080/mvc/kyselyt/1/kysymykset/').
-    then(function(response) {
-      $scope.obj = response.data;
-      
-      console.log($scope.obj);
-    });
-  
-  $scope.formData = {};
-  //post
-  $scope.submitForm = function(){
-    console.log('tulokset',$scope.formData);
-    
-    var sendData = {'id': $scope.obj.id, 'vastaukset': [
-      {'id': $scope.obj.kysymykset[0].id,
-       'kysymysid': $scope.obj.kysymykset[0].tyyppiid,
-       'teksti': null,
-       'vaihtoehdot': [
-         {'id': 2, 'teksti': $scope.formData.teksti}
-       ]
+  var myObj = {
+    "id": 1, "teksti": "HHkysely", "tyyppi": "koulukysely", "tila": "avoin", "kysymykset":
+    [
+      {"id" :1, "teksti": "Oletko kiinnostunut osallistumaan monialaiselle kurssille?", "tyyppiid": 2, "vaihtoehdot": 
+        [
+          {"id":1, "teksti": "Erittäin paljon"},
+          {"id":2, "teksti": "Jonkin verran"},
+          {"id":3, "teksti": "En juurikaan"},
+          {"id":4, "teksti": "En yhtään"}
+        ]
+      },
+      {"id": 3, "teksti": "Ajatuksia monialaisen kurssin kehittämiseksi:", "tyyppiid": 3, "vaihtoehdot": null},   
+      {"id": 2, "teksti": "Minkä koulutusohjelmien kanssa haluaisit yhteisen kurssin?", "tyyppiid": 1, "vaihtoehdot": 
+        [
+          {"id": 1, "teksti": "Finanssi- ja talousasiantuntija"},
+          {"id": 2, "teksti": "Johdon assistenttityö"},
+          {"id": 3, "teksti": "Journalismi"},
+          {"id": 4, "teksti": "Liiketalous"},
+          {"id": 5, "teksti": "Myyntityö"},
+          {"id": 6, "teksti": "Ruokatuotannon johtaminen"},
+          {"id": 7, "teksti": "Tietojenkäsittely"}
+        ]
       }
-    ]};
+      
+    ]
+  };
+  
+  //get kutsu
+//  $http.get('http://proto333.haaga-helia.fi:8080/mvc/kyselyt/1/kysymykset/').
+//    then(function(response) {
+//      $scope.obj = response.data;     
+//      console.log($scope.obj);
+//    });
+  
+  $scope.obj = myObj;
+  $scope.rbtn = myObj.kysymykset[0].vaihtoehdot;
+  $scope.cbox = myObj.kysymykset[2].vaihtoehdot;
+  
+   
+  // FORMDATA //
+  $scope.formData = {};
+  
+  $scope.submitForm = function(){
+    
+    console.log('tulokset',$scope.formData);
+       
+    var cboxArray = [];
+    
+    (function(){
+      for (var key in $scope.formData.cbox) {
+        cboxArray.push({'id': key, 'teksti': $scope.cbox[key-1].teksti});
+      }    
+    })();
+    
+    console.log(cboxArray);
+    
+    
+    var sendData = 
+      {'id': 0, 'vastaukset': 
+        [
+          {'id': 0, 'kysymysid': $scope.obj.kysymykset[0].tyyppiid, 'teksti': null, 'vaihtoehdot': 
+            cboxArray
+          },
+          {'id': 0, 'kysymysid': 3, 'teksti': 'kylla', 'vaihtoehdot': [$scope.formData.textarea]},
+          {'id': 0, 'kysymysid': 4, 'teksti': null, 'vaihtoehdot': 
+            [
+              {'id': 0, 'teksti': $scope.formData.radio}
+            ]
+          }
+        ]
+      };
     console.log(sendData);
+    
 //    $http({
 //      method : 'POST',
 //      url : 'end.html',
@@ -34,7 +84,8 @@ routerApp.controller('mainController', function($scope, $http) {
 //    })
 //   
 //    .success(function(data) { })
-  };
+    
+  };  // FORMDATA ENDS
     
   
-}); //controller ends
+}); //CONTROLLER ENDS
