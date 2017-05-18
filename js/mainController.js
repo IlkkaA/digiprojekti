e765dc64a9ac7fdc46abe01727a152305acdedd9
Alakarhu Ilkka <a1500879@myy.haaga-helia.fi>
2017-05-18 16:48:59 +0300

    snadit muutokset quest 1 ja 2. mainCtrollerissa pitäis olla nyt toimiva koodi

--- js/mainController.js
+++ js/mainController.js
@@ -1,138 +1,56 @@
 routerApp.controller('mainController', function($scope, $http) {
-  
-  var myObj = 
-{
-   "id":1,
-   "nimi":"ekaekakysely",
-   "tyyppi":"koulukysely",
-   "tila":"avoin",
-   "kysymykset":[
-      {
-         "id":1,
-         "teksti":"Oletko kiinnostunut osallistumaan monialaiselle kurssille?",
-         "tyyppiid":2,
-         "vaihtoehdot":[
-            {
-               "teksti":"Hyvin paljon",
-               "id":1
-            },
-            {
-               "teksti":"Jonkin verran",
-               "id":2
-            },
-            {
-               "teksti":"En juurikaan",
-               "id":3
-            },
-            {
-               "teksti":"En yhtään",
-               "id":4
-            },
-            {
-               "teksti":"",
-               "id":666
-            }
-         ]
-      },
-      {
-         "id":2,
-         "teksti":"Ajatuksia monialaiset opiskelijatoiminnan kehittämiseksi:",
-         "tyyppiid":3,
-         "vaihtoehdot":[
-            {
-               "teksti":null,
-               "id":0
-            }
-         ]
-      },
-      {
-         "id":3,
-         "teksti":"Minkä koulutusohjelmien kanssa haluaisit yhteisen kurssin?",
-         "tyyppiid":2,
-         "vaihtoehdot":[
-            {
-               "teksti":"Finanssi- ja talousasiantuntija",
-               "id":5
-            },
-            {
-               "teksti":"Johdon assistenttityö",
-               "id":6
-            },
-            {
-               "teksti":"Journalismi",
-               "id":7
-            },
-            {
-               "teksti":"Liiketalous",
-               "id":8
-            },
-            {
-               "teksti":"Myyntityö",
-               "id":9
-            },
-            {
-               "teksti":"Ruokatuotannon johtaminen",
-               "id":10
-            },
-            {
-               "teksti":"Tietojenkäsittely",
-               "id":11
-            }
-         ]
-      }
-   ]
-}
-  
+    
   //GET KUTSU
-//  $http.get('http://proto333.haaga-helia.fi:8080/mvc/kyselyt/1/kysymykset/').
-//    then(function(response) {
-//      $scope.obj = response.data;     
-//      console.log($scope.obj);
-//    });
-      
-  $scope.obj = myObj;
-  $scope.rbtn = $scope.obj.kysymykset[0].vaihtoehdot;
-  $scope.cbox = $scope.obj.kysymykset[2].vaihtoehdot;
+  $http.get('http://proto333.haaga-helia.fi:8080/mvc/kyselyt/1/kysymykset/').
+    then(function(response) {
+      $scope.obj = response.data;     
+      console.log(response.data);
+			$scope.rbtn = $scope.obj.kysymykset[0].vaihtoehdot;
+			$scope.cbox = $scope.obj.kysymykset[2].vaihtoehdot;
+
+			window.rbtnArrOrig = response.data.kysymykset[0].vaihtoehdot;
+			window.cboxArrOrig = response.data.kysymykset[2].vaihtoehdot;
+		
+    });
+	
   
-  var rbtnArrOrig = myObj.kysymykset[0].vaihtoehdot;
-  var cboxArrOrig = myObj.kysymykset[2].vaihtoehdot;
-  
-//  $scope.obj = myObj;
-  
-  
-   
   // FORMDATA //
   $scope.formData = {};
   
   $scope.submitForm = function(){
     
+		var cbArray = window.cboxArrOrig;
+		var rbArray = window.rbtnArrOrig;
+
     console.log("tulokset:",$scope.formData);    
     
-    // TALLENNA KLIKATTUJEN C-BOXIEN ARVOJEN ID:T JA ARVOT OBJEKTEIHIN + TAULUKKOON
     var cboxOrigId = [];
     var cboxOrigText = [];
     var cboxId = [];
     var cboxObj = {};
     var cboxArray = [];
     
+		// klikatun radiobuttonin arvo miinus 1 jotta arvo vastaa alkup. jsonin radiobutton arrayn arvoja
     var rbtnValue = $scope.formData.radiobutton - 1;
     
-    for (i = 0; i < cboxArrOrig.length; i++) {
-      cboxOrigId.push(cboxArrOrig[i].id);
-      cboxOrigText.push(cboxArrOrig[i].teksti);
+		// cboxOrigId ja cboxOrigText taulukkoon tallennetaan alkup. jsonin
+		// checkbox arrayssa olevien id ja teksti kenttien arvot
+    for (i = 0; i < cbArray.length; i++) {
+      cboxOrigId.push(cbArray[i].id);
+      cboxOrigText.push(cbArray[i].teksti);
     } 
-    
+		
+		// asetetaan taulukkoon klikattujen checkboxien arvot miinus viisi,
+		// jotta cboxOrigId arraystä saadaan suoraan haettua oikea id
     for (var key in $scope.formData.checkbox) {
       cboxId.push(key-5);
     }
-//    console.log(" klikatut cboxIdt : ", cboxId);
     
-    for (i = 0; i < cboxId.length; i++) {
-      
+		// pushataan cboxArrayhin objekteina klikattujen checkboxien id:t ja tekstit
+    for (i = 0; i < cboxId.length; i++) {  
       cboxObj = {'id': cboxOrigId[cboxId[i]], 'teksti': cboxOrigText[cboxId[i]]};
       cboxArray.push(cboxObj);
     }
-//    console.log("cboxarray:", cboxArray);
     
     
     // LÄHETTETTÄVÄ DATA PER VASTATTU KYSELY
@@ -145,13 +63,14 @@ routerApp.controller('mainController', function($scope, $http) {
           {'id': 0, 'kysymysid': 2, 'teksti': $scope.formData.textarea, 'vaihtoehdot': null},
           {'id': 0, 'kysymysid': 1, 'teksti': null, 'vaihtoehdot': 
             [
-              {'id': $scope.formData.radiobutton, 'teksti': rbtnArrOrig[rbtnValue].teksti}
+              {'id': $scope.formData.radiobutton, 'teksti': rbArray[rbtnValue].teksti}
             ]
           }
         ]
       };
     console.log("sendData:", sendData);
     
+		// LÄHETETÄÄN sendData BÄKKIIN. Pitäis toimia
     $http({
       method : 'POST',
       url : 'http://proto333.haaga-helia.fi:8080/mvc/kyselyt/1/vastaukset',
