753214cd224f47552c064f1981767a792f6833b5
Ilkka Alakarhu <ilkka_alakarhu@hotmail.com>
2017-05-18 14:27:10 +0300

    snadi muutos quest1. päivitystä paljon controlleriin, send data ok muodossa

--- js/mainController.js
+++ js/mainController.js
@@ -1,42 +1,104 @@
 routerApp.controller('mainController', function($scope, $http) {
   
-  var myObj = {
-    "id": 1, "teksti": "HHkysely", "tyyppi": "koulukysely", "tila": "avoin", "kysymykset":
-    [
-      {"id" :1, "teksti": "Oletko kiinnostunut osallistumaan monialaiselle kurssille?", "tyyppiid": 2, "vaihtoehdot": 
-        [
-          {"id":1, "teksti": "Erittäin paljon"},
-          {"id":2, "teksti": "Jonkin verran"},
-          {"id":3, "teksti": "En juurikaan"},
-          {"id":4, "teksti": "En yhtään"}
-        ]
+  var myObj = 
+{
+   "id":1,
+   "nimi":"ekaekakysely",
+   "tyyppi":"koulukysely",
+   "tila":"avoin",
+   "kysymykset":[
+      {
+         "id":1,
+         "teksti":"Oletko kiinnostunut osallistumaan monialaiselle kurssille?",
+         "tyyppiid":2,
+         "vaihtoehdot":[
+            {
+               "teksti":"Hyvin paljon",
+               "id":1
+            },
+            {
+               "teksti":"Jonkin verran",
+               "id":2
+            },
+            {
+               "teksti":"En juurikaan",
+               "id":3
+            },
+            {
+               "teksti":"En yhtään",
+               "id":4
+            },
+            {
+               "teksti":"",
+               "id":666
+            }
+         ]
       },
-      {"id": 3, "teksti": "Ajatuksia monialaisen kurssin kehittämiseksi:", "tyyppiid": 3, "vaihtoehdot": null},   
-      {"id": 2, "teksti": "Minkä koulutusohjelmien kanssa haluaisit yhteisen kurssin?", "tyyppiid": 1, "vaihtoehdot": 
-        [
-          {"id": 1, "teksti": "Finanssi- ja talousasiantuntija"},
-          {"id": 2, "teksti": "Johdon assistenttityö"},
-          {"id": 3, "teksti": "Journalismi"},
-          {"id": 4, "teksti": "Liiketalous"},
-          {"id": 5, "teksti": "Myyntityö"},
-          {"id": 6, "teksti": "Ruokatuotannon johtaminen"},
-          {"id": 7, "teksti": "Tietojenkäsittely"}
-        ]
+      {
+         "id":2,
+         "teksti":"Ajatuksia monialaiset opiskelijatoiminnan kehittämiseksi:",
+         "tyyppiid":3,
+         "vaihtoehdot":[
+            {
+               "teksti":null,
+               "id":0
+            }
+         ]
+      },
+      {
+         "id":3,
+         "teksti":"Minkä koulutusohjelmien kanssa haluaisit yhteisen kurssin?",
+         "tyyppiid":2,
+         "vaihtoehdot":[
+            {
+               "teksti":"Finanssi- ja talousasiantuntija",
+               "id":5
+            },
+            {
+               "teksti":"Johdon assistenttityö",
+               "id":6
+            },
+            {
+               "teksti":"Journalismi",
+               "id":7
+            },
+            {
+               "teksti":"Liiketalous",
+               "id":8
+            },
+            {
+               "teksti":"Myyntityö",
+               "id":9
+            },
+            {
+               "teksti":"Ruokatuotannon johtaminen",
+               "id":10
+            },
+            {
+               "teksti":"Tietojenkäsittely",
+               "id":11
+            }
+         ]
       }
-      
-    ]
-  };
+   ]
+}
   
-  //get kutsu
+  //GET KUTSU
 //  $http.get('http://proto333.haaga-helia.fi:8080/mvc/kyselyt/1/kysymykset/').
 //    then(function(response) {
 //      $scope.obj = response.data;     
 //      console.log($scope.obj);
 //    });
-  
+      
   $scope.obj = myObj;
-  $scope.rbtn = myObj.kysymykset[0].vaihtoehdot;
-  $scope.cbox = myObj.kysymykset[2].vaihtoehdot;
+  $scope.rbtn = $scope.obj.kysymykset[0].vaihtoehdot;
+  $scope.cbox = $scope.obj.kysymykset[2].vaihtoehdot;
+  
+  var rbtnArrOrig = myObj.kysymykset[0].vaihtoehdot;
+  var cboxArrOrig = myObj.kysymykset[2].vaihtoehdot;
+  
+//  $scope.obj = myObj;
+  
   
    
   // FORMDATA //
@@ -44,46 +106,65 @@ routerApp.controller('mainController', function($scope, $http) {
   
   $scope.submitForm = function(){
     
-    console.log('tulokset',$scope.formData);
-       
+    console.log("tulokset:",$scope.formData);    
+    
+    // TALLENNA KLIKATTUJEN C-BOXIEN ARVOJEN ID:T JA ARVOT OBJEKTEIHIN + TAULUKKOON
+    var cboxOrigId = [];
+    var cboxOrigText = [];
+    var cboxId = [];
+    var cboxObj = {};
     var cboxArray = [];
     
-    (function(){
-      for (var key in $scope.formData.cbox) {
-        cboxArray.push({'id': key, 'teksti': $scope.cbox[key-1].teksti});
-      }    
-    })();
+    var rbtnValue = $scope.formData.radiobutton - 1;
     
-    console.log(cboxArray);
+    for (i = 0; i < cboxArrOrig.length; i++) {
+      cboxOrigId.push(cboxArrOrig[i].id);
+      cboxOrigText.push(cboxArrOrig[i].teksti);
+    } 
+    
+    for (var key in $scope.formData.checkbox) {
+      cboxId.push(key-5);
+    }
+//    console.log(" klikatut cboxIdt : ", cboxId);
+    
+    for (i = 0; i < cboxId.length; i++) {
+      
+      cboxObj = {'id': cboxOrigId[cboxId[i]], 'teksti': cboxOrigText[cboxId[i]]};
+      cboxArray.push(cboxObj);
+    }
+//    console.log("cboxarray:", cboxArray);
     
     
+    // LÄHETTETTÄVÄ DATA PER VASTATTU KYSELY
     var sendData = 
       {'id': 0, 'vastaukset': 
         [
-          {'id': 0, 'kysymysid': $scope.obj.kysymykset[0].tyyppiid, 'teksti': null, 'vaihtoehdot': 
+          {'id': 0, 'kysymysid': 3, 'teksti': null, 'vaihtoehdot': 
             cboxArray
           },
-          {'id': 0, 'kysymysid': 3, 'teksti': 'kylla', 'vaihtoehdot': [$scope.formData.textarea]},
-          {'id': 0, 'kysymysid': 4, 'teksti': null, 'vaihtoehdot': 
+          {'id': 0, 'kysymysid': 2, 'teksti': $scope.formData.textarea, 'vaihtoehdot': null},
+          {'id': 0, 'kysymysid': 1, 'teksti': null, 'vaihtoehdot': 
             [
-              {'id': 0, 'teksti': $scope.formData.radio}
+              {'id': $scope.formData.radiobutton, 'teksti': rbtnArrOrig[rbtnValue].teksti}
             ]
           }
         ]
       };
-    console.log(sendData);
+    console.log("sendData:", sendData);
     
-//    $http({
-//      method : 'POST',
-//      url : 'end.html',
-//      data : $scope.fields,
-//      heeaders : {'Content-Type': 'application/x-www-form-urlencoded'}
-//    }).then(function(response) {
-//        data = response.data;
-//      console.log("tulokset", data);
-//    })
-//   
-//    .success(function(data) { })
+    $http({
+      method : 'POST',
+      url : 'http://proto333.haaga-helia.fi:8080/mvc/kyselyt/1/vastaukset',
+      data : sendData,
+      headers : {'Content-Type': 'application/json; charset:utf8'
+                }
+    }).then(function(response) {
+        data = response.data;
+      console.log("tulokset", data);
+    }, function errorCallback(response) {
+      console.log("ei onnistunut");
+    })
+
     
   };  // FORMDATA ENDS
     
